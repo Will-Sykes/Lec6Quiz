3ebdd134121363381dc0401ce22c55d15d738742
William Sykes <wt04s12@gmail.com>
2023-10-02 13:34:39 -0700

    added cat class on testsuit.java

--- TestSuit.java
+++ TestSuit.java
@@ -4,3 +4,13 @@ public class TestSuit{
    }
 
 }
+
+public class cat{
+   private String name;
+   private int age;
+   
+   public cat(String name, int age){
+      this.name = name;
+      this.age = age;
+   }
+}
